7e40bc491714bc4348dba109cbbbc0d03cac9811
arifurrahmanns <rof.arifrahman@gmail.com>
2025-01-29 13:59:52 +0600

    d

--- app/page.js
+++ app/page.js
@@ -132,10 +132,10 @@ export default function Home() {
               <div className="grid grid-cols-7">
                 <div className="col-span-5">
                   <h3 className="font-semibold text-black text-lg">
-                  Advance
+                    Advance
                   </h3>
                   <p className="text-sm mt-2 leading-tight text-neutral-800">
-                  Book your favourite Pro well in advance for peace of mind
+                    Book your favourite Pro well in advance for peace of mind
                   </p>
                   <button className="btn !py-1 !px-2  !text-xs mt-3">
                     Details
@@ -148,16 +148,75 @@ export default function Home() {
               <div className="grid grid-cols-7">
                 <div className="col-span-5">
                   <h3 className="font-semibold text-black text-lg">
-                  Emergency
+                    Emergency
                   </h3>
                   <p className="text-sm mt-2 leading-tight text-neutral-800">
-                  Book expert technicians for emergency repairs in minutes 
+                    Book expert technicians for emergency repairs in minutes
                   </p>
                   <button className="btn !py-1 !px-2  !text-xs mt-3">
                     Details
                   </button>
                 </div>
                 <img className="lg:col-span-2" src="/img/11906892_4867781.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="f1" className="pb-5 lg:pb-10">
+        <div className="container">
+          <div className="grid items-center lg:gap-20 lg:grid-cols-5 gap-10">
+            <img className="lg:col-span-2" src="/img/f1.svg" alt="" />
+            <div className="lg:col-span-3">
+              <h3 className="font-bold !leading-tight text-3xl lg:text-5xl text-primary">
+                Work on Your Terms , Grow on Your Schedule
+              </h3>
+              <p className="mt-5 text-base text-neutral-800">
+                Join HelpRush and take charge of your earnings. Offer services when it suits
+                you and grow your business on your terms. No resources required
+                from us—just your skills and dedication.
+              </p>
+              <div className="flex gap-5 items-center mt-5 lg:mt-10">
+                <a href="" className="btn !bg-theme2  !text-primary">Get Started </a>
+                <a href="" className="border-b-2  border-black pb-1">Already have an account?Sign in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="f2" className="pb-5 lg:pb-10">
+        <div className="container">
+          <div className="grid items-center lg:gap-20 lg:grid-cols-5 gap-10">
+            <img className="lg:order-2 lg:col-span-2" src="/img/f2.svg" alt="" />
+            <div className="lg:order-1 lg:col-span-3">
+              <h3 className="font-bold !leading-tight text-3xl lg:text-5xl text-primary">
+                Turn everyday Tasks into earnings with errands
+              </h3>
+              <p className="mt-5 text-base text-neutral-800">
+                From grocery runs to bill payments ,help your community while earning extra income.Join HelpRush today and get paid for making life easier in Kashmir.
+              </p>
+              <div className="flex gap-5 items-center mt-5 lg:mt-10">
+                <a href="" className="btn !bg-theme2  !text-primary">Get Started </a>
+                <a href="" className="border-b-2  border-black pb-1">Already have an account?Sign in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="f1" className="pb-10 lg:pb-20">
+        <div className="container">
+          <div className="grid items-center lg:gap-20 lg:grid-cols-5 gap-10">
+            <img className="lg:col-span-2" src="/img/f3.svg" alt="" />
+            <div className="lg:col-span-3">
+              <h3 className="font-bold !leading-tight text-3xl lg:text-5xl text-primary">
+                HelpRush is designed with business in Mind
+              </h3>
+              <p className="mt-5 text-base text-neutral-800">
+                HelpRush is built with businesses in mind, offering a platform where you can connect with thousands of new customers while seamlessly managing your teams, boosting productivity, and organizing work. With smart data insights to drive growth and social benefits for your team, HelpRush empowers your business to thrive.
+              </p>
+              <div className="flex gap-5 items-center mt-5 lg:mt-10">
+                <a href="" className="btn !bg-theme2  !text-primary">Get Started </a>
+                <a href="" className="border-b-2  border-black pb-1">Already have an account?Sign in</a>
               </div>
             </div>
           </div>
